f9f017d8c9e430d5cdbc20c3a44b9052e4567006
Aman <aman.prajapati7022@gmail.com>
2024-07-05 12:04:00 +0530

    searching works

---
+++ src/getDetails.js
@@ -0,0 +1,65 @@
+
+const {API_KEY} = process.env;
+const baseUrl = 'http://api.weatherapi.com/v1/forecast.json';
+
+function getReqDetails(res){
+    // console.log(res.current.condition);
+    const details = {current:{}, hour: []};
+
+    //current detials for top-right 
+    details.current.text = res.current.condition.text;
+    details.current.location = res.location.name+', '+res.location.country;
+    details.current.datetime = res.location.localtime;
+    details.current.temp_c = res.current.temp_c;
+    details.current.temp_f = res.current.temp_f;
+    details.current.icon = res.current.condition.icon;
+
+    //current details for top-left
+    details.current.feelslike_c = res.current.feelslike_c;
+    details.current.feelslike_f = res.current.feelslike_f;
+    details.current.humidity = res.current.humidity;
+    details.current.chance_of_rain = res.forecast.forecastday[0].day.daily_chance_of_rain;
+    details.current.wind_kph = res.current.wind_kph;
+    details.current.wind_mph = res.current.wind_mph;
+
+    let i =0;
+    let curHour = new Date(details.current.datetime).getHours()+1;
+    let day = 0;
+
+    while(i<8){
+        let hourInfo = {};
+        if(curHour == 24){
+            curHour = 0;
+            day++;
+        }
+        hourInfo.datetime = res.forecast.forecastday[day].hour[curHour].time;
+        hourInfo.temp_c = res.forecast.forecastday[day].hour[curHour].temp_c;
+        hourInfo.temp_f = res.forecast.forecastday[day].hour[curHour].temp_f;
+        hourInfo.icon = res.forecast.forecastday[day].day.condition.icon;
+        details.hour.push(hourInfo);
+        i++;
+        curHour++;
+    }
+    return details;
+}
+
+async function getforecast(city){
+    const url = baseUrl+`?key=${API_KEY}&q=${city}&days=3`;
+    console.log(url);
+    try{
+        const resp = await fetch(url);
+        if(resp.status == 400){
+            alert('no such city');
+        }
+        else if(resp.status == 200){
+            const res = await resp.json();
+            let reqInfo = getReqDetails(res);
+            return reqInfo;
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+export default getforecast;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,27 @@
 import './styles.css'
 
-const {API_KEY} = process.env;
-const baseUrl = 'http://api.weatherapi.com/v1/forecast.json';
+import getforecast from './getDetails';
+import render from './render';
 
 
 
-function getReqDetails(res){
-    // console.log(res.current.condition);
-    const details = {current:{}, days: []};
-
-    //current detials for top-right 
-    details.current.text = res.current.condition.text;
-    details.current.location = res.location.name+', '+res.location.country;
-    details.current.datetime = res.location.localtime_epoch;
-    details.current.temp_c = res.current.temp_c;
-    details.current.temp_f = res.current.temp_f;
-    details.current.icon = res.current.condition.icon;
-
-    //current details for top-left
-    details.current.feelslike_c = res.current.feelslike_c;
-    details.current.feelslike_f = res.current.feelslike_f;
-    details.current.humidity = res.current.humidity;
-    details.current.chance_of_rain = res.forecast.forecastday[0].day.daily_chance_of_rain;
-    details.current.wind_kph = res.current.wind_kph;
-    details.current.wind_mph = res.current.wind_mph;
-
+async function search(city){
     
-    for(i of [0,1,2]){
-        let dayInfo = {};
-        dayInfo.datetime = res.forecast.forecastday[i].date_epoch;
-        dayInfo.maxtemp_c = res.forecast.forecastday[i].day.maxtemp_c;
-        dayInfo.maxtemp_f = res.forecast.forecastday[i].day.maxtemp_f;
-        dayInfo.mintemp_c = res.forecast.forecastday[i].day.mintemp_c;
-        dayInfo.mintemp_f = res.forecast.forecastday[i].day.mintemp_f;
-        dayInfo.icon = res.forecast.forecastday[i].day.condition.icon;
-        details.days.push(dayInfo);
-    }
-    return details;
-}
-
-async function getforecast(city){
-    const url = baseUrl+`?key=${API_KEY}&q=${city}&days=3`;
-    try{
-        const resp = await fetch(url);
-        if(resp.status == 400){
-            alert('no such city');
-        }
-        else if(resp.status == 200){
-            const res = await resp.json();
-            let reqInfo = getReqDetails(res);
-            return reqInfo;
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-(async function  (){
     console.log('start loading');
-    // let reqInfo = await getforecast('new delhi');
-    // console.log(reqInfo);
+    let reqInfo = await getforecast(city);
+    console.log(reqInfo);
+    render(reqInfo);
     console.log('stop loading');
 
-})();
+    localStorage.setItem('location', city);
+}
+
+
+
+if(localStorage.getItem('location')){
+    search(localStorage.getItem('location'));
+}
+else 
+    search('new delhi');
+
+export {search};
